1851dc6afb10d83661a98f04e0ca177d0a087dfb
Gergo Foldhazi <gergo.foldhazi@gmail.com>
2019-04-11 20:45:44 +0200

    initial commint
    - project created
    - initial folder structure created

---
+++ darksky/BackendService/DarkSky.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace darksky.BackendService
+{
+    public class DarkSky
+    {
+        // dark sky api key: f1be72a5165735c754a9cbf6bbd205a7
+        // must supply in all api calls
+        // example call 
+        // GET https://api.darksky.net/forecast/f1be72a5165735c754a9cbf6bbd205a7/37.8267,-122.423
+    }
+}
